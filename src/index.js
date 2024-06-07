f1a0f8a9102de05ef4f69fe4dc1198f228ac2590
Goal Ogunsina <gunsin.goal@gmail.com>
2024-06-07 12:46:13 +0100

    Many new Features

--- src/index.js
+++ src/index.js
@@ -18,53 +18,64 @@ class App {
       y: this.tileSize / 2,
       size: 30,
       angle: 0,
-      locked: false
+      locked: false,
+      sprite: new Sprite("../images/happy.webp"),
     };
-    this.point = {
-      x: Math.floor(Math.random() * this.tilesX) * this.tileSize + this.tileSize / 2,
-      y: Math.floor(Math.random() * this.tilesY) * this.tileSize + this.tileSize / 2,
-      size: 10,
+    this.winPoint = {
+      x: 14 * this.tileSize + this.tileSize / 2,
+      y: 2 * this.tileSize + this.tileSize / 2,
+      radius: 10,
     };
     this.lasers = [new Laser()];
-    this.laserLimit = 10
+    this.laserLimit = 10;
     this.latestLaserIdx = 0;
     this.mirrors = [
-      { x: 300, y: 200, width: 50, height: 10, angle: deg2Rad(60) },
-      { x: 500, y: 300, width: 50, height: 10, angle: deg2Rad(-45) },
-      {
-        x: Math.floor(Math.random() * this.tilesX) * this.tileSize,
-        y: Math.floor(Math.random() * this.tilesY) * this.tileSize,
-        width: 50,
-        height: 10,
-        angle: deg2Rad(-45),
-      },
+      ...JSON.parse(
+        '[{"x":300,"y":200,"angle":1.0471975511965976},{"x":500,"y":300,"angle":-0.7853981633974483},{"x":500,"y":350,"angle":-0.7853981633974483},{"x":0,"y":250,"angle":2.6216379752221353},{"x":600,"y":300,"angle":1.4190343904351879},{"x":0,"y":350,"angle":0.7487850303082835},{"x":150,"y":450,"angle":0.713105628539875},{"x":600,"y":500,"angle":1.6235140681879212},{"x":750,"y":500,"angle":0.4594571772192553},{"x":650,"y":550,"angle":0.25881054648883556}]'
+      ),
+      { x: 14 * this.tileSize, y: 2 * this.tileSize, angle: deg2Rad(90) },
+      { x: 14 * this.tileSize, y: 3 * this.tileSize, angle: deg2Rad(90) },
+      { x: 14 * this.tileSize, y: 4 * this.tileSize, angle: deg2Rad(-70) },
+      { x: 15 * this.tileSize, y: 4 * this.tileSize, angle: deg2Rad(-70) },
     ];
+    // this.mirrors = [
+    //   { x: 300, y: 200, width: 50, height: 10, angle: deg2Rad(60) },
+    //   { x: 500, y: 300, width: 50, height: 10, angle: deg2Rad(-45) },
+    //   {
+    //     x: Math.floor(Math.random() * this.tilesX) * this.tileSize,
+    //     y: Math.floor(Math.random() * this.tilesY) * this.tileSize,
+    //     width: 50,
+    //     height: 10,
+    //     angle: deg2Rad(-45),
+    //   },
+    // ];
     // console.log(this.tilesX);
-    for (let i = 0; i < this.tilesY + 1; i++) {
-      for (let j = 0; j < this.tilesX + 1; j++) {
-        if (Math.round(Math.random() * 10) !== 0) {
-          continue;
-        }
-        // const element = array[j];
-        this.mirrors.push({
-          x: j * this.tileSize,
-          y: i * this.tileSize,
-          width: 50,
-          height: 10,
-          angle: deg2Rad(Math.random() * 180),
-        });
-        // console.log('d');
-      }
-      // this.mirrors.push({
-      //   x: Math.floor(Math.random() * this.tilesX) * this.tileSize,
-      //   y: Math.floor(Math.random() * this.tilesY) * this.tileSize,
-      //   width: 50,
-      //   height: 10,
-      //   angle: deg2Rad(Math.random() * 180),
-      // });
-    }
-    this.steelWalls = [{ x: 200, y: 400, width: 50, height: 10 }];
+    // for (let i = 0; i < this.tilesY + 1; i++) {
+    //   for (let j = 0; j < this.tilesX + 1; j++) {
+    //     if (Math.round(Math.random() * 10) !== 0) {
+    //       continue;
+    //     }
+    //     // const element = array[j];
+    //     this.mirrors.push({
+    //       x: j * this.tileSize,
+    //       y: i * this.tileSize,
+    //       width: 50,
+    //       height: 10,
+    //       angle: deg2Rad(Math.random() * 180),
+    //     });
+    //     // console.log('d');
+    //   }
+    //   // this.mirrors.push({
+    //   //   x: Math.floor(Math.random() * this.tilesX) * this.tileSize,
+    //   //   y: Math.floor(Math.random() * this.tilesY) * this.tileSize,
+    //   //   width: 50,
+    //   //   height: 10,
+    //   //   angle: deg2Rad(Math.random() * 180),
+    //   // });
+    // }
     this.gameStartTime = Date.now();
+    this.mirrorHeight = 10;
+    this.mirrorWidth = 50;
     this.init();
   }
 
@@ -78,11 +89,10 @@ class App {
 
   handleMouseMove(event) {
     if (!this.player.locked) {
-      
-    const rect = this.canvas.getBoundingClientRect();
-    const mouseX = event.clientX - rect.left;
-    const mouseY = event.clientY - rect.top;
-    this.player.angle = Math.atan2(mouseY - this.player.y, mouseX - this.player.x);
+      const rect = this.canvas.getBoundingClientRect();
+      const mouseX = event.clientX - rect.left;
+      const mouseY = event.clientY - rect.top;
+      this.player.angle = Math.atan2(mouseY - this.player.y, mouseX - this.player.x);
     }
   }
 
@@ -102,11 +112,11 @@ class App {
         },
         this.player.angle
       );
-      this.player.locked = true
+      this.player.locked = true;
     } else {
       this.lasers[this.latestLaserIdx].release();
       this.latestLaserIdx++;
-      this.player.locked = false
+      this.player.locked = false;
     }
   }
 
@@ -116,10 +126,48 @@ class App {
     this.drawPlayer();
     this.drawPoint();
     this.drawMirrors();
-    this.drawSteelWalls();
     this.updateLaserMirrorBounce();
     this.lasers.forEach((laser) => laser.render(this.ctx));
+    this.deleteUnecessaryLasers();
+    this.updateWinStatus();
     requestAnimationFrame(this.update.bind(this));
+  }
+
+  deleteUnecessaryLasers() {
+    let uselessLasers = [];
+    this.lasers.forEach((laser, i) => {
+      if (this.isLaserOutOfSight(laser)) {
+        uselessLasers.push(i);
+      }
+    });
+    this.deleteLasers(uselessLasers);
+  }
+
+  deleteLasers(idxs) {
+    idxs.forEach((idx) => {
+      this.lasers.splice(idx, 1);
+      this.latestLaserIdx -= 1;
+    });
+  }
+
+  updateWinStatus() {
+    for (let i = 0; i < this.lasers.length; i++) {
+      const laser = this.lasers[i];
+      if (laser.points) {
+        let lastpoint = laser.points[laser.points.length - 1];
+        if (
+          lineCircIntersection(
+            [
+              { x: lastpoint.x, y: lastpoint.y },
+              { x: laser.x, y: laser.y },
+            ],
+            { x: this.winPoint.x, y: this.winPoint.y, r: this.winPoint.radius }
+          )
+        ) {
+          alert("you win");
+        }
+      }
+    }
   }
 
   drawTiles() {
@@ -145,12 +193,20 @@ class App {
     this.ctx.lineTo(7, 0);
     this.ctx.lineTo(-7, 0);
     this.ctx.closePath();
-    this.ctx.fillStyle = "red";
+    this.ctx.fillStyle = "green";
     this.ctx.fill();
-    this.ctx.font = `${this.player.size}px Arial`;
-    this.ctx.textAlign = "center";
-    this.ctx.textBaseline = "middle";
-    this.ctx.fillText("😃", 0, 0);
+    if (this.player.sprite.loaded) {
+      this.ctx.drawImage(
+        this.player.sprite.img,
+        -this.player.sprite.img.width / 2,
+        -this.player.sprite.img.height / 2
+      );
+    } else {
+      this.ctx.font = `${this.player.size}px Arial`;
+      this.ctx.textAlign = "center";
+      this.ctx.textBaseline = "middle";
+      this.ctx.fillText("😃", 0, 0);
+    }
 
     this.ctx.restore();
   }
@@ -158,7 +214,7 @@ class App {
   drawPoint() {
     this.ctx.fillStyle = "red";
     this.ctx.beginPath();
-    this.ctx.arc(this.point.x, this.point.y, this.point.size, 0, Math.PI * 2);
+    this.ctx.arc(this.winPoint.x, this.winPoint.y, this.winPoint.radius, 0, Math.PI * 2);
     this.ctx.fill();
   }
 
@@ -169,24 +225,12 @@ class App {
       this.ctx.translate(mirror.x, mirror.y);
       this.ctx.rotate(mirror.angle);
       this.ctx.fillRect(
-        -mirror.width / 2,
-        -mirror.height / 2,
-        mirror.width,
-        mirror.height
+        -this.mirrorWidth / 2,
+        -this.mirrorHeight / 2,
+        this.mirrorWidth,
+        this.mirrorHeight
       );
       this.ctx.restore();
-    });
-  }
-
-  drawSteelWalls() {
-    this.ctx.fillStyle = "gray";
-    this.steelWalls.forEach((wall) => {
-      this.ctx.fillRect(
-        wall.x - wall.width / 2,
-        wall.y - wall.height / 2,
-        wall.width,
-        wall.height
-      );
     });
   }
 
@@ -223,17 +267,9 @@ class App {
   getLaserMirrorCollision(laser) {
     return this.mirrors.find((mirror) => {
       return isPointInPolygon(
-        box2points(mirror.x, mirror.y, mirror.width, mirror.height, mirror.angle),
+        box2points(mirror.x, mirror.y, this.mirrorWidth, this.mirrorHeight, mirror.angle),
         { x: laser.x, y: laser.y }
       );
-    });
-  }
-
-  checkSteelWallCollision(laser) {
-    return this.steelWalls.some((wall) => {
-      const dx = laser.x - wall.x;
-      const dy = laser.y - wall.y;
-      return Math.abs(dx) <= wall.width / 2 && Math.abs(dy) <= wall.height / 2;
     });
   }
 
@@ -241,7 +277,7 @@ class App {
     // if (Date.now() - this.gameStartTime >= 1000) {
     //   this.laser.bounce(deg2Rad(-45))
     // }
-    this.lasers.forEach(laser => {
+    this.lasers.forEach((laser) => {
       let mirror = this.getLaserMirrorCollision(laser);
       if (mirror) {
         const intersectionData = getIntersectionData(
@@ -259,8 +295,8 @@ class App {
             x: mirror.x,
             y: mirror.y,
             r: mirror.angle,
-            width: mirror.width,
-            height: mirror.height,
+            width: this.mirrorWidth,
+            height: this.mirrorHeight,
           }
         );
         if (intersectionData) {
@@ -285,7 +321,7 @@ class App {
           laser.points[0].moving = true;
         }
       }
-      })
+    });
   }
 
   reflectAngle(reflectAngle, baseAngle) {
@@ -293,6 +329,13 @@ class App {
     // return reflectAngle + Math.PI - 2 * baseAngle + Math.PI;
   }
 
+  isLaserOutOfSight(laser = this.lasers[0]) {
+    if (laser.points) {
+      return laser.points.every((p) => {
+        return p.x < 0 || p.x > this.canvas.width || p.y < 0 || p.y > this.canvas.height;
+      });
+    }
+  }
 }
 
 class Laser {
@@ -322,7 +365,10 @@ class Laser {
     p.x += Math.cos(p.angle) * this.speed * p.moving;
     p.y += Math.sin(p.angle) * this.speed * p.moving;
   }
-
+  /**
+   *
+   * @param {CanvasRenderingContext2D} ctx
+   */
   renderLaser(ctx) {
     ctx.save();
     ctx.beginPath();
@@ -334,13 +380,31 @@ class Laser {
       this.directPoint(p);
     }
     ctx.lineTo(this.x, this.y);
-    ctx.strokeStyle = "red";
+    ctx.strokeStyle = "green";
+    ctx.lineWidth = 2;
+    ctx.lineJoin = "bevel";
     ctx.stroke();
+    ctx.shadowColor = "green";
+    ctx.shadowBlur = 12;
+    ctx.s;
     ctx.restore();
   }
   bounce(angle) {
     this.points.push({ angle: angle, moving: false, x: this.x, y: this.y });
     this.angle = angle;
+  }
+}
+
+class Sprite {
+  constructor(imgsrc, anonymousCrossorigin) {
+    this.img = new Image();
+    this.img.src = imgsrc;
+    if (anonymousCrossorigin == null || anonymousCrossorigin === true) {
+      this.img.crossOrigin = "anonymous";
+    }
+    this.img.onload = (() => {
+      this.loaded = true;
+    }).bind(this);
   }
 }
 
