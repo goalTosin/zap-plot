59a935e8faa0a484c238e2c9d95e53b2c1ed9180
Goal Ogunsina <gunsin.goal@gmail.com>
2024-06-09 17:57:00 +0100

    Done some level generation mechanisms

--- other/generateLevels.js
+++ other/generateLevels.js
@@ -8,6 +8,34 @@ function generateLevel(
   wrongMirrorCount
 ) {
   // generate a random winpoint
+  const winPoint = genRandWinpoint(tileSize, tilesX, tilesY);
+  // instanciate mirrors
+  const mirrors = [];
   // generate mirrors that direct point the laser at the right point
+  // mirrors.push(...genMirrors(laserStartX, laserStartY, winPoint))
   // generate mirrors that confuse the player and doesn't direct the laser up at the right point
+  // const wrongWinPoint = {
+  //   x: Math.random())
+  // }
+  // mirrors.push(...genRandWinpoint())
+  return { winPoint, mirrors}
+}
+
+function genRandWinpoint(tileSize, tilesX, tilesY) {
+  const randP = (t) => tileSize + Math.floor(Math.random() * (t - 1)) * tileSize;
+  return {
+    x: randP(tilesX),
+    y: randP(tilesY),
+    radius: 10,
+  };
+}
+
+function genMirrors(x = NaN, y = NaN, mirrors = -1, winPoint) {
+  for (let i = 0; i < mirrors; i++) {
+    
+  }
+}
+function reflectAngle(reflectAngle, baseAngle) {
+  return baseAngle * 2 + Math.PI - reflectAngle;
+  // return reflectAngle + Math.PI - 2 * baseAngle + Math.PI;
 }
